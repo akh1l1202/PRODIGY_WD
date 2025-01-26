86bc06a8402361e12a8660b7d4d8867be7fe4e4b
akhil <147427374+akh1l1202@users.noreply.github.com>
2025-01-26 18:38:04 +0530

    Completed Task-1

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
